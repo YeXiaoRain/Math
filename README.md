2e6e9777cecd712a05047c9e025887fec7af74be
cromarmot <yexiaorain@gmail.com>
2019-04-23 14:49:07 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Math
 ====
 
+## INIT
 
 > DOWNLOAD
 
@@ -13,3 +14,29 @@ Math
 > clone theme
 
 `git clone https://github.com/iissnan/hexo-theme-next themes/next`
+
+> repo for gh-pages
+
+```
+mkdir public
+cd public
+git add origin git@yxrgithub:YeXiaoRain/Math.git
+git fetch
+git checkout -b gh-pages origin/gh-pages
+```
+
+**config yxrgithub in `~/.ssh/config`**
+
+## USE
+
+> local server
+
+`hexo s`
+
+> generate
+
+`hexo g`
+
+> update code & generate
+
+`./update.sh`
---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : cromarmot (yexiaorain@gmail.com)
+# @file        : update
+# @created     : 星期二 4月 23, 2019 14:41:11 CST
+#
+# @description : 
+######################################################################
+
+git add . && git commit -m "update" && git push origin gh-pages-dev
+hexo g && cd public && git add . && git commit -m "update" && git push origin gh-pages && cd ../
